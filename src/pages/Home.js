ec2bc4948d658d6e4ae183e660265a110230e9ae
hcweigand10 <henryweigand10@gmail.com>
2022-09-07 17:28:00 -0700

    more home styliing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,15 @@ const Home = () => {
         <div>
             <div class="px-4 py-5 text-light" id="home-background">
                 <div class="row flex-lg-row align-items-center g-5 py-5">
+                    <div className="col-md-10 col-lg-6 text-center">
+                        <img
+                            className=""
+                            src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/70467972_3030519450298432_7694635722707304448_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=XriFcnaqKQAAX9UQOk4&_nc_ht=scontent-sea1-1.xx&oh=00_AT9VWbeq6EiGiwI1UL3WILm5LGljDY4BhT2qKnidPu15aQ&oe=63409088"
+                            alt="Peter Headshot"
+                            id="profile-pic"
+                            style={{ objectFit: "cover", maxWidth: "65%" }}
+                        />
+                    </div>
                     <div class="col-lg-6">
                         <h1 class="display-5 fw-bold lh-1 mb-3 home-title">
                             Peter Strasser
@@ -22,45 +31,31 @@ const Home = () => {
                     <span class="pulse"></span>
                 </div>
             </div>
-            <div class="container px-4 py-5" id="featured-3">
-                <section className="py-4" id="about">
+            <div class="col-md-10 col-lg-11 row px-4 py-5 mx-auto">
+                <section className="py-4 col-xl-4" id="about">
                     <h2 className="text-center section-heading mb-1">About</h2>
                     <div className="d-flex justify-content-center">
                         <hr
                             style={{ width: "60px", border: "1px solid gray" }}
                         />
                     </div>
-                    <div className="row my-1 mx-auto p-3">
-                        <div className="col-md-4 col-lg-6 text-center p-3">
-                            <img
-                                className=""
-                                src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/70467972_3030519450298432_7694635722707304448_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=XriFcnaqKQAAX9UQOk4&_nc_ht=scontent-sea1-1.xx&oh=00_AT9VWbeq6EiGiwI1UL3WILm5LGljDY4BhT2qKnidPu15aQ&oe=63409088"
-                                alt="Peter Headshot"
-                                id="profile-pic"
-                                style={{ objectFit: "cover", maxWidth: "80%" }}
-                            />
-                        </div>
-                        <div className="col-md-8 col-lg-6 p-3">
-                            <div className="pt-3 h-100">
-                                <h5 className="text-muted">Peter Strasser</h5>
-                                <p className="plain-text">
-                                    Exercitation nostrud ut tempor velit duis
-                                    dolore minim mollit. In consectetur dolore
-                                    deserunt ut consequat dolor est irure qui.
-                                    Dolore ut ex et voluptate ut officia ex est.
-                                </p>
-                                <p>
-                                    Cillum est duis id ad. Duis duis irure aute
-                                    officia et ex id mollit magna. Proident
-                                    excepteur eu ex cillum amet sit pariatur.
-                                    Excepteur anim laborum ea duis laboris
-                                    deserunt.
-                                </p>
-                            </div>
-                        </div>
+                    <div className="pt-3 h-100">
+                        <h5 className="text-muted">Peter Strasser</h5>
+                        <p className="plain-text">
+                            Exercitation nostrud ut tempor velit duis dolore
+                            minim mollit. In consectetur dolore deserunt ut
+                            consequat dolor est irure qui. Dolore ut ex et
+                            voluptate ut officia ex est.
+                        </p>
+                        <p>
+                            Cillum est duis id ad. Duis duis irure aute officia
+                            et ex id mollit magna. Proident excepteur eu ex
+                            cillum amet sit pariatur. Excepteur anim laborum ea
+                            duis laboris deserunt.
+                        </p>
                     </div>
                 </section>
-                <section className="py-4">
+                <section className="py-4 col-xl-4">
                     <h2 className="text-center section-heading mb-1">Rates</h2>
                     <div className="d-flex justify-content-center">
                         <hr
@@ -118,7 +113,7 @@ const Home = () => {
                         </div>
                     </div>
                 </section>
-                <section className="pt-4 mb-3">
+                <section className="py-4 col-xl-4">
                     <h2 className="text-center section-heading mb-1">
                         Contact
                     </h2>
@@ -127,16 +122,8 @@ const Home = () => {
                             style={{ width: "60px", border: "1px solid gray" }}
                         />
                     </div>
-                    <div class="row justify-content-center">
-                        <div class="col-md-3 text-center">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <i class="fas fa-map-marker-alt mt-4 fa-2x"></i>
-                                    <p>Friday Harbor, WA</p>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-3 text-center">
+                    <div class="row justify-content-center w-100">
+                        <div class="col-md-6 text-center">
                             <ul class="list-unstyled mb-0">
                                 <li>
                                     <i class="fas fa-phone mt-4 fa-2x"></i>
@@ -144,7 +131,7 @@ const Home = () => {
                                 </li>
                             </ul>
                         </div>
-                        <div class="col-md-3 text-center">
+                        <div class="col-md-6 text-center">
                             <ul class="list-unstyled mb-0">
                                 <li>
                                     <i class="fas fa-envelope mt-4 fa-2x"></i>
