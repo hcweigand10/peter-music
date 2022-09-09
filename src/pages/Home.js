741bc2f35a1fcf7f31182f750cc14585711d0ad8
hcweigand10 <henryweigand10@gmail.com>
2022-09-09 13:04:31 -0700

    steelblue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -61,7 +61,7 @@ const Home = () => {
                     <h4 className="text-center">$40 per session</h4>
                     <div
                         className="row my-1 mx-auto p-3"
-                        style={{ width: "350px" }}
+                        style={{ width: "320px" }}
                     >
                         <div className="">
                             <span style={{ float: "left" }}>
